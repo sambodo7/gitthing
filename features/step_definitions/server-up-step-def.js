cc01e431bbfc88eaab0615fd331633255ad0189f
bodo <sstephenson@kana.com>
2017-03-24 12:08:46 +0000

    nearly have features

--- features/step_definitions/server-up-step-def.js
+++ features/step_definitions/server-up-step-def.js
@@ -1,4 +1,10 @@
-module.exports = function () {
+/*eslint-env node*/
+/*eslint-disable new-cap*/
+
+const { defineSupportCode } = require( "cucumber" );
+
+defineSupportCode( function( { When, Then } ) {
+
     When('I make a request to the server', function (callback) {
         // Write code here that turns the phrase above into concrete actions
         callback(null, 'pending');
@@ -13,4 +19,5 @@ module.exports = function () {
         // Write code here that turns the phrase above into concrete actions
         callback(null, 'pending');
     } );
-}
+
+} );
