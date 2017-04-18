135c924a8f24885c3f54867af351d95daee41644
sambodo <ssteven352@gmail.com>
2017-04-19 00:25:24 +0100

    stuff

--- features/step_definitions/server-up-step-def.js
+++ features/step_definitions/server-up-step-def.js
@@ -5,14 +5,18 @@ const { defineSupportCode } = require( "cucumber" );
 
 defineSupportCode( function( { Given, When, Then } ) {
 
-    When('I make a request to the server', function (callback) {
+    When( "I make a request to the server", function (callback) {
+
         this.browser.visit( "/", callback );
+
     } );
     
-    Then('I should recieve a status {int} back', function (arg1, callback) {
-        // Write code here that turns the phrase above into concrete actions
-        callback(null, 'pending');
-    });
+    Then( "I should recieve a status {int} back", function ( code, callback ) {
+        
+        this.browser.assert.status( code );
+        callback();
+
+    } );
 
     Then('I should recieve a html document', function (callback) {
         // Write code here that turns the phrase above into concrete actions
