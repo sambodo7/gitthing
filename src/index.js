8ecb7bb5cd186953b82ab90f2d2448f36fa76790
sambodo <ssteven352@gmail.com>
2017-04-23 19:29:57 +0100

    using handlebars

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 const express = require("express");
-const app = express();
+const path = require("path");
+const exphbs  = require("express-handlebars");
 const config = require("./config");
+const app = express();
+
+var hbs = exphbs.create( {
+
+    defaultLayout: "main",
+    extname: ".html",
+    layoutsDir:"src/views/layouts"
+
+} );
+
+app.engine("html", exphbs( hbs.engine ) );
+app.set( "view engine", "html" );
+app.set("views", "src/views");
+
+app.use( express.static( path.join( __dirname, "public" ) ) );
 
 app.get( "/", (req, res) => {
 
-    res.send("Hello World!");
+    res.render( "body", {
+        title: "Main page"
+    } );
 
 } );
 
