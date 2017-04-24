32f31552a9ff7c8d4c5fffc05938e26e53200810
sambodo <ssteven352@gmail.com>
2017-04-24 08:26:35 +0100

    output at least

--- src/index.js
+++ src/index.js
@@ -8,20 +8,20 @@ var hbs = exphbs.create( {
 
     defaultLayout: "main",
     extname: ".html",
-    layoutsDir:"src/views/layouts"
+    layoutsDir:  path.join( __dirname, "views/layouts" )
 
 } );
 
 app.engine("html", exphbs( hbs.engine ) );
 app.set( "view engine", "html" );
-app.set("views", "src/views");
+app.set("views", path.join( __dirname, "views" ) );
 
 app.use( express.static( path.join( __dirname, "public" ) ) );
 
 app.get( "/", (req, res) => {
 
     res.render( "body", {
-        title: "Main page"
+        title: "Main page",
     } );
 
 } );
